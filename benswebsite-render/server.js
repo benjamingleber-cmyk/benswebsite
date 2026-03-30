a73d0f22d598ac97a1076f64447f5dafc4219500
benjamingleber-cmyk <benjamin.gleber@gmail.com>
2026-03-30 00:57:44 -0400

    Add files via upload

---
+++ benswebsite-render/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { createServer } from "http";
+import { createBareServer } from "@tomphttp/bare-server-node";
+import { createWispServer } from "wisp-server-node";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+const bare = createBareServer("/bare/");
+
+app.use(express.static(join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "public", "index.html"));
+});
+
+const server = createServer();
+
+server.on("request", (req, res) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else if (req.url.startsWith("/wisp/")) {
+    createWispServer({ server }).handleRequest(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Ben's Website running on port ${PORT}`);
+});
